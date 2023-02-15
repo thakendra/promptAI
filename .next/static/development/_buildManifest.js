b307a2cb101c09e98eeea6c957b0b5fe94403879
thakendra <69754309+thakendra@users.noreply.github.com>
2023-02-15 19:56:02 +0545

    Delete .next directory

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/gifts":["static\u002Fchunks\u002Fpages\u002Fgifts.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fgifts"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
